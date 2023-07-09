fd2445b3d44ee89d32315500854023eb6ccf8c99
Birch <birchtree1205@gmail.com>
2023-07-09 12:41:09 -0700

    update to 7-8-23
    update to 7-8-23

--- index.ts
+++ index.ts
@@ -1,3 +1,25 @@
 // Please start your own codes from here!
+import { bedrockServer } from "bdsx/launcher";
+import { events } from "bdsx/event";
+import { BackupManager } from "@bdsx/backup/BackupManager";
+
+const backupManager = new BackupManager(bedrockServer, events);
+backupManager.init({
+    backupOnStart: false,
+    skipIfNoActivity: true,
+    backupOnPlayerConnected: false,
+    backupOnPlayerDisconnected: true,
+    interval: 120,
+    minIntervalBetweenBackups: 120,
+}).then((res) => {
+    console.log(`backup manager initiated`);
+});
+// Please start your own codes from here!
+
+//import './example_and_test'; // remove this if it's not necessary for you
+
+// Please start your own codes from here!
+
+//import './example_and_test'; // remove this if it's not necessary for you
 
 import "./example_and_test"; // remove this if it's not necessary for you
